cf48f6c3e6a88b458d50f4d0a544ca5810c19ac2
immanoj16 <mmaharana14@gmail.com>
2017-07-24 15:43:23 +0530

    Add a method to the java file for team a

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -13,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(8);
     }
 
-    
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
 }
