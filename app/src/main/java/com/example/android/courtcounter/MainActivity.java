f9e3e53575811438724c88b4821510025c8c88a9
immanoj16 <mmaharana14@gmail.com>
2017-07-24 16:02:43 +0530

    Add score point methods

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,17 +2,33 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
     }
 
+    public void addThreeForTeamA(View view) {
+        score += 3;
+        displayForTeamA(score);
+    }
+
+    public void addTwoForTeamA(View view) {
+        score += 2;
+        displayForTeamA(score);
+    }
+
+    public void addOneForTeamA(View view) {
+        score += 1;
+        displayForTeamA(score);
+    }
     /**
      * Displays the given score for Team A.
      */
